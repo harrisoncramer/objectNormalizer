1e9829e93e3e7588dc8f4d437c19a400aa8f17e5
Harrison Cramer <harrisoncramer@gmail.com>
2021-09-03 08:31:21 -0700

    Refactored to make code more DRY.

--- normalize.js
+++ normalize.js
@@ -1,63 +1,68 @@
 const util = require('util');
 const { data, data2, data3, data4 } = require('./data');
 
-function normalizeResult(result) {
-  const rootQueryObject = {};
-  const denestedObjects = [];
+const rootQueryObject = {};
+const denestedObjects = [];
 
-  /* If top level result is an array, denest every object in it.
-   * If it's an object, start denesting immediately.
-   */
+/* If top level result is an array, denest every object in it.
+ * If it's an object, start denesting immediately.
+ */
+function normalizeResult(result) {
   for (const key in result) {
     rootQueryObject[key] = `loql__${key}`;
     const value = result[key];
     if (Array.isArray(value)) {
-      rootQueryObject[key] = value.map((subArrayVal, i) => {
-        const uniqueKey = key + '_' + subArrayVal.id; // Add id...
-        return flattenAndWrite({ object: subArrayVal, parentKey: uniqueKey });
-      });
+      rootQueryObject[key] = flattenArray({ value, childKey: key });
     } else {
-      flattenAndWrite({ object: result[key], parentKey: key });
+      flatObjAndWrite({ object: result[key], parentKey: key });
     }
-  }
-
-  /*
-   * Takes an object, and a parent key. Recursively goes through keys
-   * in object, and writes all to denestedObjects array. If result at key is a
-   * primitive, write to result. If object, set to result of recursive call.
-   * If array, set to mapped value of array.
-   */
-  function flattenAndWrite({ object, parentKey }) {
-    const result = {};
-    for (const key in object) {
-      const value = object[key];
-      if (Array.isArray(value)) {
-        let childKey = parentKey + '_' + key;
-        result[key] = value.map((subArrayVal) => {
-          if (typeof subArrayVal === 'object' && subArrayVal !== null) {
-            if (!subArrayVal.id) return subArrayVal;
-            const uniqueKey = childKey + '_' + subArrayVal.id; // Add id...
-            return flattenAndWrite({
-              object: subArrayVal,
-              parentKey: uniqueKey,
-            });
-          }
-          return subArrayVal;
-        });
-      } else if (typeof value === 'object' && value !== null) {
-        let childKey = parentKey + '_' + key;
-        result[key] = flattenAndWrite({ object: value, parentKey: childKey });
-      } else {
-        result[key] = value;
-      }
-    }
-
-    denestedObjects.push({ [parentKey]: result });
-
-    return 'loql__' + parentKey;
   }
 
   return { rootQueryObject, denestedObjects };
+}
+
+/*
+ * Takes an object, and a parent key. Recursively goes through keys
+ * in object, and writes all to denestedObjects array. If result at key is a
+ * primitive, write to result. If object, set to result of recursive call.
+ * If array, set to mapped value of flattenedArray.
+ */
+function flatObjAndWrite({ object, parentKey }) {
+  const result = {};
+  for (const key in object) {
+    const value = object[key];
+    if (Array.isArray(value)) {
+      let childKey = parentKey + '_' + key;
+      result[key] = flattenArray({ value, childKey });
+    } else if (typeof value === 'object' && value !== null) {
+      let childKey = parentKey + '_' + key;
+      result[key] = flatObjAndWrite({ object: value, parentKey: childKey });
+    } else {
+      result[key] = value;
+    }
+  }
+
+  denestedObjects.push({ [parentKey]: result });
+  return 'loql__' + parentKey;
+}
+
+/*
+ * Takes an array, loops over every value in the array.
+ * For every object, calls flattenObject, then returns the ref.
+ * For every non-object, returns the value directly.
+ */
+function flattenArray({ value, childKey }) {
+  return value.map((subArrayVal) => {
+    if (typeof subArrayVal === 'object' && subArrayVal !== null) {
+      if (!subArrayVal.id) return subArrayVal;
+      const uniqueKey = childKey + '_' + subArrayVal.id; // Add id...
+      return flatObjAndWrite({
+        object: subArrayVal,
+        parentKey: uniqueKey,
+      });
+    }
+    return subArrayVal;
+  });
 }
 
 const res = normalizeResult(data2.data);
